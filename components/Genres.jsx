64266d822c5a0daa2e7ddc7b0056de7e94ccd2ec
M Zia <murtaza_zia@hotmail.co.uk>
2024-03-21 20:11:34 +0000

    worked on the Genres component, added the image and styling

--- components/Genres.jsx
+++ components/Genres.jsx
@@ -1,7 +1,10 @@
+import Image from "next/image"
 
 function Genres() {
   return (
-    <div className="flex">Genres</div>
+    <div className="flex items-center justify-center pt-5 pb-11">
+      <Image src="/genres/genres.png" width={927} height={40} className="mx-auto" />
+    </div>
   )
 }
 
