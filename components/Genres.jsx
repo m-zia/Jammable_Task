1a980d3d745c126af3fbc6cd2042d92ebf3d1f2b
M Zia <murtaza_zia@hotmail.co.uk>
2024-03-23 16:43:52 +0000

    completed Genres component

--- components/Genres.jsx
+++ components/Genres.jsx
@@ -1,34 +1,19 @@
-import Image from "next/image";
 import GenresBadge from "./GenresBadge";
 
 function Genres() {
   return (
-
     <>
       <div className="flex gap-2 pt-4 pb-9 flex-wrap sm:justify-center">
-
-      
-      
-      <GenresBadge image="/Goku.png" text="Music"/>
-
-      <GenresBadge image="/Goku.png" text="Cartoon"/>
-      <GenresBadge image="/Goku.png" text="Anime"/>
-      <GenresBadge image="/Goku.png" text="Gaming"/>
-      <GenresBadge image="/Goku.png" text="German"/>
-      <GenresBadge image="/Goku.png" text="Korean"/>
-
-      <GenresBadge image="/Goku.png" text="French"/>
-
-      <GenresBadge image="/Goku.png" text="Public Figure"/>
-
-
+        <GenresBadge image="/genres/weekend.png" text="Music" />
+        <GenresBadge image="/genres/sponge.png" text="Cartoon" />
+        <GenresBadge image="/genres/vegeta.png" text="Anime" />
+        <GenresBadge image="/genres/sonic.png" text="Gaming" />
+        <GenresBadge image="/genres/german.png" text="German" />
+        <GenresBadge image="/genres/Korean.png" text="Korean" />
+        <GenresBadge image="/genres/macron.png" text="French" />
+        <GenresBadge image="/genres/trump.png" text="Public Figure" />
       </div>
     </>
-
-
-
-
-
   )
 }
 
